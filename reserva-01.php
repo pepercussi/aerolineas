22650a2c76932266dbc87eac4ee28f85f39e39b0
Guido <guidopardo@gmail.com>
2014-11-17 19:37:14 -0300

    V006
    Agregué la imágen de las alas

--- reserva-01.php
+++ reserva-01.php
@@ -78,8 +78,13 @@
 				</div>
 
 				<div class="row separador01"></div>
-
 				
+				<!-- Ahora viene el dibujo del avion -->
+				<div class="row rowFiltro00">
+					<div class="col-md-4 text-right"><img class="img-responsive" src="media/img/wing_left.png" /></div>
+					<div class="col-md-4"></div>
+					<div class="col-md-4 text-left"><img class="img-responsive" src="media/img/wing_right.png" /></div>
+				</div>
 			</form>
 		</div>
 	</body>
